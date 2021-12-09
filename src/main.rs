15b8440902b45568d8fb2f4bfa720a4d22ec74b0
Edmundo Paulino <manyymoore@gmail.com>
2021-12-09 11:56:32 -0300

    style: 📱 Work on resposiveness

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,26 @@ fn window_conf() -> Conf {
     }
 }
 
+fn smallest_screen() -> f32 {
+    let height = screen_height();
+    let width = screen_width();
+    if height < width {
+        height
+    } else {
+        width
+    }
+}
+
 #[macroquad::main(window_conf)]
 async fn main() {
     loop {
         clear_background(WHITE);
 
+        // draw_triangle(v1, v2, v3, color)
         draw_circle(
             screen_width() / 2.0,
             screen_height() / 2.0,
-            screen_height() / 4.0,
+            smallest_screen() / 4.0,
             RED,
         );
 
