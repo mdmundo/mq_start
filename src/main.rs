6c9fae0af0e55f7de85d4b1cd8c0013a85d79adf
Edmundo Paulino <manyymoore@gmail.com>
2021-12-09 10:57:44 -0300

    refactor: 🏗️ Japan flag

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Bandeira do Japão")]
+async fn main() {
+    loop {
+        clear_background(WHITE);
+
+        draw_circle(
+            screen_width() / 2.0,
+            screen_height() / 2.0,
+            screen_height() / 4.0,
+            RED,
+        );
+
+        next_frame().await
+    }
 }
